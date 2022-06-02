d8450ee3ff4617cbd998c1911d8aaa1a3d9e1959
Daniel Msika <danieldou@hotmail.fr>
2022-06-02 02:14:46 -0400

    update prod apiurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  apiUrl: 'https://test.thekitah.com',
   stripePublicKey: 'pk_live_51KfyDdBw2bxMO5tdm4EHqCRAKwNZ4kItcyUloT8THPb4hcYfCBkCljM3zzvkqIjEB82vJygs24tRTdP4SOfm5kbl00eY7AKuMK'
 };
