5877a372e6cad6aafe3ea66a96b02c604b504575
Daniel Msika <danieldou@hotmail.fr>
2022-06-13 16:41:19 -0400

    fix account layout title

--- src/app/modules/account/pages/account-layout/account-layout.component.ts
+++ src/app/modules/account/pages/account-layout/account-layout.component.ts
@@ -16,17 +16,10 @@ export class AccountLayoutComponent {
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router,
     private authService: AuthService
   ) {
-    router.events.subscribe(e => {
-      if (e instanceof NavigationEnd) {
-        console.log(route.root.firstChild)
-        console.log(this.route?.root?.firstChild?.snapshot.data);
-      }
-    });
-    this.title$ = this.route.data.pipe(
-      map(data => data['title'])
+    this.title$ = this.route.url.pipe(
+      map(() => this.route.snapshot?.firstChild?.data['title'])
     );
     this.loggedInUser$ = this.authService.loggedInUser$.pipe(
       map(apiAuthResponse => apiAuthResponse ?? null),
