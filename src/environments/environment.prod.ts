e2dcde19883b1a95ee3fe7cc2e233140a1d71998
Daniel Msika <danieldou@hotmail.fr>
2022-06-14 01:52:51 -0400

    stripe test key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.thekitah.com',
-  stripePublicKey: 'pk_live_51KfyDdBw2bxMO5tdm4EHqCRAKwNZ4kItcyUloT8THPb4hcYfCBkCljM3zzvkqIjEB82vJygs24tRTdP4SOfm5kbl00eY7AKuMK'
+  //stripePublicKey: 'pk_live_51KfyDdBw2bxMO5tdm4EHqCRAKwNZ4kItcyUloT8THPb4hcYfCBkCljM3zzvkqIjEB82vJygs24tRTdP4SOfm5kbl00eY7AKuMK',
+  stripePublicKey: 'pk_test_51KfyDdBw2bxMO5tdA9NV0RhWcoWplqEeHJlINQhmSonK1jDFQ4s4WVMbA3H3ZgNTeObZTAxfrecoWf9gI3zDRtbq00IPUOTcs7'
 };
