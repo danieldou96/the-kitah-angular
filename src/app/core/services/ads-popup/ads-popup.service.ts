6ce48c1df3ce88a8bb20a2bce9d827e3765bf452
Daniel Msika <danieldou@hotmail.fr>
2023-01-20 16:26:50 -0500

    change cta popup

--- src/app/core/services/ads-popup/ads-popup.service.ts
+++ src/app/core/services/ads-popup/ads-popup.service.ts
@@ -16,19 +16,19 @@ export class AdsPopupService {
 
   openAdsPopup() {
     setTimeout(() => {
-      if (this.cookieService.get('chanukahGiftShowed') == 'true') {
+      if (this.cookieService.get('popupShowed') == 'true') {
         return;
       }
       this.dialog.open(AdsPopupComponent, {
         data: {
-          title: 'Hey, we’ve got something special for you!',
-          description: 'Don’t miss this exclusive Chanukah download',
+          title: 'The whatsapp status for teachers!',
+          description: '<p>Join our whatsapp status to see new daily uploads: <a href="https://wa.me/message/KZ4EUKPXZGLZC1" target="_blank">Join our whatsapp status to see new daily uploads</a><p><p>Or check us out on <a href="https://www.instagram.com/the.kitah/" target="_blank">Instagram</a>',
           buttonLabel: '',
-          declineText: 'No thanks, I don’t want this awesome offer'
+          declineText: 'No thanks'
         }
       });
-      // 5 days
-      this.cookieService.set('chanukahGiftShowed', 'true', 432000000, '/');
+      // 10 days
+      this.cookieService.set('popupShowed', 'true', 864000000, '/');
     }, 5000);
   }
 }
